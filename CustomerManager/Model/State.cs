e464c3f10678075f5c55426a3a4e9dda8b6d9f07
Dan Wahlin <dwahlin@hotmail.com>
2013-09-11 15:49:50 -0700

    Updating image

--- CustomerManager/Model/State.cs
+++ CustomerManager/Model/State.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,7 +9,9 @@ namespace CustomerManager.Model
     public class State
     {
         public int Id { get; set; }
+        [StringLength(2)]
         public string Abbreviation { get; set; }
+        [StringLength(25)]
         public string Name { get; set; }
     }
 }
