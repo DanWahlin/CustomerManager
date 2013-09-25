7195da870fe7ea391f20a6b0849ff152c9625020
Dan Wahlin <dwahlin@xmlforasp.net>
2013-09-25 12:43:56 -0700

    Minor modification to the directive to check if the value has changed or not when we're checking if it's unique.

--- CustomerManager/app/directives/wcUnique.js
+++ CustomerManager/app/directives/wcUnique.js
@@ -10,10 +10,17 @@ define(['app'], function (app) {
                 element.bind('blur', function (e) {
                     if (!ngModel || !element.val()) return;
                     var keyProperty = scope.$eval(attrs.wcUnique);
-                    dataService.checkUniqueValue(keyProperty.key, keyProperty.property, element.val())
+                    var currentValue = element.val();
+                    dataService.checkUniqueValue(keyProperty.key, keyProperty.property, currentValue)
                         .then(function (unique) {
-                            ngModel.$setValidity('unique',  unique);
+                            //Ensure value that being checked hasn't changed
+                            //since the Ajax call was made
+                            if (currentValue == element.val()) {
+                                ngModel.$setValidity('unique', unique);
+                            }
                         }, function () {
+                            //Probably want a more robust way to handle an error
+                            //For this demo we'll set unique to true though
                             ngModel.$setValidity('unique', true);
                         });
                 });
