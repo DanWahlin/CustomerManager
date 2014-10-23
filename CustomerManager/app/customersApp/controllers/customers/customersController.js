53b42d1475f0f941eb6a0428d1bc2d43d9e495c4
Dan Wahlin <dwahlin@xmlforasp.net>
2014-10-23 12:37:14 -0700

    Was missing "." on the custom animation class. Added it.

--- CustomerManager/app/customersApp/controllers/customers/customersController.js
+++ CustomerManager/app/customersApp/controllers/customers/customersController.js
@@ -14,7 +14,7 @@ define(['app'], function (app) {
         $scope.orderby = 'lastName';
         $scope.reverse = false;
         $scope.searchText = null;
-        $scope.cardAnimationClass = 'card-animation';
+        $scope.cardAnimationClass = '.card-animation';
 
         //paging
         $scope.totalRecords = 0;
